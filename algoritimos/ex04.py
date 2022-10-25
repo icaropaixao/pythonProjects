525e5617ffa763fc0a2ece84077f370df4b692a3
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-10-25 20:13:02 -0300

    exercício 4

---
+++ algoritimos/ex04.py
@@ -0,0 +1,10 @@
+# faça um algoritmo que peça dois numero e imprima a soma
+
+# entradas
+num1 = int(input("Informe o primeiro numero: "))
+num2 = int(input("Informe o segundo numero: "))
+
+#processamento
+soma = num1 + num2
+
+print ("O resultado da soma dos numeros é: {0}".format(soma))

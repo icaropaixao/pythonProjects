57219b42c160765eb7f0c80fd7f7668102cff426
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-10-25 19:51:05 -0300

    exercicios 1 e 2 de algoritomos em py

---
+++ algoritimos1/ex02.py
@@ -0,0 +1,11 @@
+# faça um algoritimo para somar dois numeros e multiplicar o resultado pelo primeiro numero.
+
+# entradas
+num1 = int(input("informe o primeiro numero: "))
+num2 = int(input("informe o segundo numero:"))
+# processamento
+soma = num1 + num2
+multiplicacao = soma * num1
+
+# saida
+print("O resultado da multiplicacao é{0}".format(multiplicacao))

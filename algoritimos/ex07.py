756f5b64aa8b44389f2c22370fa6e6b422c0be97
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-10-27 20:38:17 -0300

    algoritmo 7

---
+++ algoritimos/ex07.py
@@ -0,0 +1,10 @@
+# Tendo como dados de entrada a altura de uma pessoa
+# construa um algotirmo que calcula seu peso ideal
+# usando a seguinte formula
+# (72.7 * altura) - 58
+ 
+altura = float(input("Informe sua altura: "))
+
+peso_ideal = (72.7 * altura) - 58
+
+print("Seu peso ideal é {0}", peso_ideal)
